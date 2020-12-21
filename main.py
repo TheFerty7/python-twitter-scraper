a3b827c22bf2bc7c3983398868fa21289d8c4328
TheFerty7 <i-meric@hotmail.com>
2020-12-20 18:45:09 -0800

    Bug fixes and chrome driver option

--- main.py
+++ main.py
@@ -10,19 +10,25 @@ def start_parse(conn):
 	with_replies = False
 	if input('Do you want to include replies? (y/n): ') == 'y':
 		with_replies = True
+	else:
+		with_replies = False
 
 	retweets = False
 	if input('Do you want to include retweets? (y/n): ') == 'y':
 		retweets = True
+	else:
+		retweets = False
 
 	pages_to_scroll = int(input('Number of pages to scroll: '))
 
-	#TODO: select driver type, currently just assumes Firefox with GekoDriver is available
+	#TODO: select driver type, need to add more drivers
+	driver_type = input('Which driver do you want to use? (firefox, chrome): ')
+
 	username_id = db_connection.get_id_by_username(conn, username)
 	if username_id is None:
 		username_id = db_connection.insert_username(conn, username)
 
-	set_of_tweets = scraper.start_scrape(username, pages_to_scroll, with_replies, retweets)
+	set_of_tweets = scraper.start_scrape(username, pages_to_scroll, driver_type, with_replies, retweets)
 	db_connection.insert_loop_tweet_urls(conn, list(set_of_tweets), username_id)
 
 if __name__ == '__main__':
